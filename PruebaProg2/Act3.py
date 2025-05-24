da1554252ab1c6edcb9cab7ac46744c276117f31
wetalff <alfredito.osejo@gmail.com>
2025-05-23 18:15:41 -0600

    Ejercicio numero 3 de la Actividad evaluada

--- PruebaProg2/Act3.py
+++ PruebaProg2/Act3.py
@@ -2,3 +2,22 @@
 #arreglo unidimensional. Existen tres técnicas para realizar este proceso: por filas, por
 #columnas o en zigzag. Crear un programa que permita la linealización de un arreglo
 #bidimensional por columnas. Los datos del arreglo bidimensional serán tomados de la tabla.
+
+
+Matriz = [[10, 50, 40], 
+         [30, 20, 60], 
+         [70, 80, 90]]
+
+print("-" * 22)
+for fila in Matriz:
+    for columna in fila:
+        print(f"|{columna:> 5}", end = " ")
+    print("|")
+    print("-"*22)
+    
+print("Matriz Alineada por columnas")
+
+print("-" * 36)
+resultado = [Matriz[fila][col] for col in range(len(Matriz[0])) for fila in range(len(Matriz))]
+print(resultado)
+print("-" * 36)
